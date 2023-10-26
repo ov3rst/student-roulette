e233bf2033c475ccb34932fe209a2bea1a1dd258
Oliver Tejeda <oliver@hot.com>
2023-10-25 23:25:27 -0400

    creating a function on display names, name centering feature pending completion

--- Program.cs
+++ Program.cs
@@ -22,9 +22,9 @@ namespace student_roulette
         {
             while (!exit)
             {
-                WriteLine("Roulette");
-                getStudents();
-                WriteLine($"{firstStudent}, {secondStudent}");
+                //WriteLine("Roulette");
+                //getStudents();
+                DrawResult(5, 12);
                 ReadKey(true);
             }
         }
@@ -37,7 +37,6 @@ namespace student_roulette
                 //Write("Enter para continuar...");
                 repeatedNumbers.Clear();
                 CompleteMenu();
-                //if (ReadKey(true).Key == ConsoleKey.Escape) return;
             }
 
             Random randomStudent = new();
@@ -137,6 +136,37 @@ namespace student_roulette
             });
 
             return currentSelection;
+        }
+
+        static void DrawResult(int liveDeveloper, int facilitator)
+        {
+            string first = CenterName(students[liveDeveloper], 23), second = CenterName(students[facilitator], 25);
+            WriteLine("╔═══════════════════════╦═════════════════════════╗");
+            WriteLine("║ Desarrollador en vivo ║ Facilitador de ejecicio ║");
+            WriteLine("╠═══════════════════════╬═════════════════════════╣");
+            WriteLine($"║{first}║{second}║");
+            Write("╚═══════════════════════╩═════════════════════════╝");
+        }
+
+        //Esta funcion lo que hace es centrarme el nombre en el recuadro hecho añadiendo los espacios necesarios para que quede centrado.
+        //Hay que arreglar el cuando el total de caracteres de un nombre no es par (quitando obviamente el espacio entre apellidos). queda eso pendiente en esta funcion.
+        static string CenterName(string name, int space)
+        {
+            int whiteSpaces = space - name.Length;
+            string spaces = "";
+
+            for (int i = 0; i < whiteSpaces; i++)
+            {
+                if (spaces.Length == whiteSpaces / 2)
+                {
+                    name = name.Insert(0, spaces);
+                    name = name.Insert(name.Length, spaces);
+                    break;
+                }
+                spaces += " ";
+            }
+
+            return name;
         }
     }
 }

a0765de10517c5655843ed55610df2e7375d3c76
Oliver Tejeda <oliver@hot.com>
2023-10-26 23:09:10 -0400

    Added animations

--- Program.cs
+++ Program.cs
@@ -2,44 +2,51 @@
 
 namespace student_roulette
 {
+    //Agregar un cronometro que pueda ser elegido, por ejemplo podemos preguntar iniciando el programa si deseamos usar solamente el cronometro, tambien despues de haber elegido los estudiantes preguntar si se quiere ahi mismo cronometrar el desafio.
+    // Agregar un reloj, para ver la hora actual en el programa, que se pueda elegir si mostrar o no.
     internal class Program
     {
         private static string[] completionMenuOptions = {
             "Reiniciar Ruleta", "Salir del Programa"
         };
-        private static int x, y;
-
 
         private static string[] students = {
         "Juan Arias", "Pedro Diaz", "Maria Cuevas", "Oliver Tejeda", "Julio Perez","Juanita Contreras", "Wilfredo Carvajal", "Rafael Montas", "Camila Sierra", "Ernesto Lopez", "Francisco Gomez", "Manuel Almonte", "John Smith", "Yonatan Aquino","Jose Soto", "Orison Guerrero"
         };
 
+        //private static int x, y;
         private static List<int> repeatedNumbers = new();
-        private static bool exit = false;
-
+        private static bool exit = false, withAnimation = true;
         private static int firstStudent, secondStudent;
+
         static void Main(string[] args)
         {
+            CursorVisible = false;
             while (!exit)
             {
-                //WriteLine("Roulette");
-                //getStudents();
-                DrawResult(5, 12);
+                Clear();
+                //if (!Login()) continue;
+                GetStudents();
+                DrawResult(firstStudent, secondStudent);
                 ReadKey(true);
             }
         }
 
-        static void getStudents()
+        static void GetStudents()
         {
-            if (repeatedNumbers.Count() == students.Length)
+            Clear();
+            if (repeatedNumbers.Count == students.Length)
             {
                 WriteLine("Ya han participado todos los estudiantes reiniciando ruleta...");
                 //Write("Enter para continuar...");
                 repeatedNumbers.Clear();
                 CompleteMenu();
+                LoadingAnimation("\r\n ██▀███  ▓█████  ██▓ ███▄    █  ██▓ ▄████▄   ██▓ ▄▄▄       ███▄    █ ▓█████▄  ▒█████  \r\n▓██ ▒ ██▒▓█   ▀ ▓██▒ ██ ▀█   █ ▓██▒▒██▀ ▀█  ▓██▒▒████▄     ██ ▀█   █ ▒██▀ ██▌▒██▒  ██▒\r\n▓██ ░▄█ ▒▒███   ▒██▒▓██  ▀█ ██▒▒██▒▒▓█    ▄ ▒██▒▒██  ▀█▄  ▓██  ▀█ ██▒░██   █▌▒██░  ██▒\r\n▒██▀▀█▄  ▒▓█  ▄ ░██░▓██▒  ▐▌██▒░██░▒▓▓▄ ▄██▒░██░░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█▄   ▌▒██   ██░\r\n░██▓ ▒██▒░▒████▒░██░▒██░   ▓██░░██░▒ ▓███▀ ░░██░ ▓█   ▓██▒▒██░   ▓██░░▒████▓ ░ ████▓▒░\r\n░ ▒▓ ░▒▓░░░ ▒░ ░░▓  ░ ▒░   ▒ ▒ ░▓  ░ ░▒ ▒  ░░▓   ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒▓  ▒ ░ ▒░▒░▒░ \r\n  ░▒ ░ ▒░ ░ ░  ░ ▒ ░░ ░░   ░ ▒░ ▒ ░  ░  ▒    ▒ ░  ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒   ░ ▒ ▒░ \r\n  ░░   ░    ░    ▒ ░   ░   ░ ░  ▒ ░░         ▒ ░  ░   ▒      ░   ░ ░  ░ ░  ░ ░ ░ ░ ▒  \r\n   ░        ░  ░ ░           ░  ░  ░ ░       ░        ░  ░         ░    ░        ░ ░  \r\n                                   ░                                  ░               \r\n", withAnimation);
+                Clear();
             }
 
             Random randomStudent = new();
+            LoadingAnimation("\r\n  ▄████ ▓█████  ███▄    █ ▓█████  ██▀███   ▄▄▄       ███▄    █ ▓█████▄  ▒█████  \r\n ██▒ ▀█▒▓█   ▀  ██ ▀█   █ ▓█   ▀ ▓██ ▒ ██▒▒████▄     ██ ▀█   █ ▒██▀ ██▌▒██▒  ██▒\r\n▒██░▄▄▄░▒███   ▓██  ▀█ ██▒▒███   ▓██ ░▄█ ▒▒██  ▀█▄  ▓██  ▀█ ██▒░██   █▌▒██░  ██▒\r\n░▓█  ██▓▒▓█  ▄ ▓██▒  ▐▌██▒▒▓█  ▄ ▒██▀▀█▄  ░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█▄   ▌▒██   ██░\r\n░▒▓███▀▒░▒████▒▒██░   ▓██░░▒████▒░██▓ ▒██▒ ▓█   ▓██▒▒██░   ▓██░░▒████▓ ░ ████▓▒░\r\n ░▒   ▒ ░░ ▒░ ░░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒▓  ▒ ░ ▒░▒░▒░ \r\n  ░   ░  ░ ░  ░░ ░░   ░ ▒░ ░ ░  ░  ░▒ ░ ▒░  ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒   ░ ▒ ▒░ \r\n░ ░   ░    ░      ░   ░ ░    ░     ░░   ░   ░   ▒      ░   ░ ░  ░ ░  ░ ░ ░ ░ ▒  \r\n      ░    ░  ░         ░    ░  ░   ░           ░  ░         ░    ░        ░ ░  \r\n                                                                ░               \r\n", withAnimation);
 
             while (true)
             {
@@ -58,6 +65,7 @@ namespace student_roulette
             repeatedNumbers.Add(secondStudent);
         }
 
+        //Hay que abstraer la funcionalidad de elegir opcion en el menu para poder utilizarla en mas sitios.(Por ejemplo para elegir si queremos o no animaciones).
         static void CompleteMenu()
         {
             bool loop = true;
@@ -66,12 +74,7 @@ namespace student_roulette
 
             CursorVisible = false;
 
-            WriteLine("¿Desea continuar?");
-
-            x = CursorLeft;
-            y = CursorTop;
-
-            Menu(completionMenuOptions, selectedOption);
+            DrawMenu(completionMenuOptions, selectedOption);
 
             while (loop)
             {
@@ -90,9 +93,8 @@ namespace student_roulette
                             selectedOption--;
                             break;
                     }
-                    CursorLeft = x;
-                    CursorTop = y;
-                    Menu(completionMenuOptions, selectedOption);
+
+                    DrawMenu(completionMenuOptions, selectedOption);
                 }
 
                 loop = false;
@@ -108,12 +110,13 @@ namespace student_roulette
             }
         }
 
-        private static string Menu(string[] options, int optionResult)
+        private static string DrawMenu(string[] options, int optionResult)
         {
             string currentSelection = "";
             int destacado = 0;
 
             Clear();
+            WriteLine("¿Desea continuar?\n");
             Array.ForEach(options, element =>
             {
                 if (destacado == optionResult)
@@ -148,25 +151,97 @@ namespace student_roulette
             Write("╚═══════════════════════╩═════════════════════════╝");
         }
 
-        //Esta funcion lo que hace es centrarme el nombre en el recuadro hecho añadiendo los espacios necesarios para que quede centrado.
-        //Hay que arreglar el cuando el total de caracteres de un nombre no es par (quitando obviamente el espacio entre apellidos). queda eso pendiente en esta funcion.
+        //Esta funcion lo que hace es centrar el nombre en el recuadro hecho añadiendo los espacios necesarios para que quede bien centrado.
         static string CenterName(string name, int space)
         {
-            int whiteSpaces = space - name.Length;
+            int whiteSpaces = space - name.Length, nameLenght = name.Length;
             string spaces = "";
 
-            for (int i = 0; i < whiteSpaces; i++)
+            for (int i = 0; i <= whiteSpaces; i++)
             {
-                if (spaces.Length == whiteSpaces / 2)
+                if (spaces.Length == whiteSpaces / 2 && name.Length == nameLenght)
                 {
                     name = name.Insert(0, spaces);
+                    spaces = "";
+                }
+                else if (i == whiteSpaces)
+                {
                     name = name.Insert(name.Length, spaces);
-                    break;
                 }
                 spaces += " ";
             }
 
             return name;
+        }
+
+        //Falta validar si el usuario deja el campo vacio y tambien que no se pase de la logitud permitida.
+        private static bool Login()
+        {
+            string userName = string.Empty, password = string.Empty;
+            LoadingAnimation("\r\n ▄▄▄▄    ██▓▓█████  ███▄    █ ██▒   █▓▓█████  ███▄    █  ██▓▓█████▄  ▒█████  \r\n▓█████▄ ▓██▒▓█   ▀  ██ ▀█   █▓██░   █▒▓█   ▀  ██ ▀█   █ ▓██▒▒██▀ ██▌▒██▒  ██▒\r\n▒██▒ ▄██▒██▒▒███   ▓██  ▀█ ██▒▓██  █▒░▒███   ▓██  ▀█ ██▒▒██▒░██   █▌▒██░  ██▒\r\n▒██░█▀  ░██░▒▓█  ▄ ▓██▒  ▐▌██▒ ▒██ █░░▒▓█  ▄ ▓██▒  ▐▌██▒░██░░▓█▄   ▌▒██   ██░\r\n░▓█  ▀█▓░██░░▒████▒▒██░   ▓██░  ▒▀█░  ░▒████▒▒██░   ▓██░░██░░▒████▓ ░ ████▓▒░\r\n░▒▓███▀▒░▓  ░░ ▒░ ░░ ▒░   ▒ ▒   ░ ▐░  ░░ ▒░ ░░ ▒░   ▒ ▒ ░▓   ▒▒▓  ▒ ░ ▒░▒░▒░ \r\n▒░▒   ░  ▒ ░ ░ ░  ░░ ░░   ░ ▒░  ░ ░░   ░ ░  ░░ ░░   ░ ▒░ ▒ ░ ░ ▒  ▒   ░ ▒ ▒░ \r\n ░    ░  ▒ ░   ░      ░   ░ ░     ░░     ░      ░   ░ ░  ▒ ░ ░ ░  ░ ░ ░ ░ ▒  \r\n ░       ░     ░  ░         ░      ░     ░  ░         ░  ░     ░        ░ ░  \r\n      ░                           ░                          ░               \r\n", withAnimation);
+
+            CursorVisible = true;
+            DrawLogin();
+            CursorTop = 1;
+            CursorLeft = 13;
+            userName = ReadLine()!.ToLower();
+
+            CursorTop = 3;
+            CursorLeft = 13;
+            password = HidePassword();
+            CursorVisible = true;
+            return userName == "ov3rst" && password == "123456";
+        }
+
+        private static void DrawLogin()
+        {
+            WriteLine("╔══════════╦══════════╗");
+            WriteLine("║  Login   ║          ║");
+            WriteLine("╠══════════╬══════════╣");
+            WriteLine("║ Password ║          ║");
+            Write("╚══════════╩══════════╝");
+        }
+
+        private static string HidePassword()
+        {
+            string password = string.Empty;
+            while (true)
+            {
+                var key = ReadKey(true);
+                if (key.Key == ConsoleKey.Enter) break;
+
+                password += Convert.ToString(key.KeyChar);
+                Write("*");
+            }
+
+            return password;
+        }
+
+        private static void LoadingAnimation(string prompt, bool withAnimation)
+        {
+            const byte time = 50;
+            Clear();
+            if (withAnimation)
+            {
+                Write(prompt);
+                SetCursorPosition(10, 15);
+                ForegroundColor = ConsoleColor.White;
+                for (int i = 0; i < time; i++)
+                {
+                    Write("█");
+                }
+
+                SetCursorPosition(10, 15);
+                ForegroundColor = ConsoleColor.Green;
+                for (int i = 0; i < time; i++)
+                {
+                    Write("█");
+                    Thread.Sleep(50);
+                }
+
+                Clear();
+                ResetColor();
+            }
         }
     }
 }
